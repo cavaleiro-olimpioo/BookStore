4ddc6589b16b2c6c8e22aa4f52808652e35de9ab
Guilherme Olimpio <guilhermearnaldi@gmail.com>
2026-07-19 22:21:16 -0300

    CreateAutor
    Criação da tabela de autor

--- src/main/java/com/olimpio/bookstore/models/AutorModel.java
+++ src/main/java/com/olimpio/bookstore/models/AutorModel.java
@@ -1,4 +1,26 @@
 package com.olimpio.bookstore.models;
 
-public class AutorModel {
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_AUTOR")
+public class AutorModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private String name;
+
+    public void setId(UUID id) { this.id = id; }
+    public UUID getId() { return id; }
+
+    public void setName(String name) { this.name = name; }
+    public String getName() { return name; }
 }
