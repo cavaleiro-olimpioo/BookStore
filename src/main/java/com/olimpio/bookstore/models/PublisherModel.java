d49f9957b3537c9677facb17015c605593e3e3c6
Guilherme Olimpio <guilhermearnaldi@gmail.com>
2026-07-19 21:45:05 -0300

    CreatePublisher
    Criação da tabela de publicador

--- src/main/java/com/olimpio/bookstore/models/PublisherModel.java
+++ src/main/java/com/olimpio/bookstore/models/PublisherModel.java
@@ -1,4 +1,25 @@
 package com.olimpio.bookstore.models;
 
-public class PublisherModel {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PUBLISHER")
+public class PublisherModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private String name;
+
+    public void setId(UUID id) { this.id = id; }
+    public UUID getId() { return id; }
+
+    public void setName(String name) { this.name = name; }
+    public String getName() { return name; }
 }
